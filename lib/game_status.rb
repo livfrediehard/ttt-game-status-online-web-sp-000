94c1c4947fa694a120dac629e1062108916b2eae
Brian Palmer <bpalmer710@gmail.com>
2021-06-09 00:38:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,15 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,4,8], # Across left
+  [2,4,6], # Across right
+  [0,3,6], # left
+  [2,5,8], # Right
+  [1,4,7]  # Middle
+  
+  def won? (board)
+    
